81d25bb69e88ec17ad2328b12458a938ae9ec433
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-04-06 19:14:45 -0400

    updated task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -27,7 +27,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 		parent->right->parent = new;
 
-	parent->left = new;
+	parent->right = new;
 
 	return (new);
 }
